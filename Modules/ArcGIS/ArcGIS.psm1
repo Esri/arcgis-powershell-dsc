6892c61486e590eb582e8390e65e7bb96d357ef7
Shailesh Goel <SGoel@esri.com>
2018-04-25 11:05:32 -0700

    Removes Requirement of SSL-Certs being Configured on each machine

--- Modules/ArcGIS/ArcGIS.psm1
+++ Modules/ArcGIS/ArcGIS.psm1
@@ -563,12 +563,16 @@ function Configure-ArcGIS
             $ConfigurationParamsHashtable.AllNodes[$i].WMFVersion = $WMFVersion
             if($HasSSLCertificatesPerNode -and -not($ConfigurationParamsHashtable.AllNodes[$i].SslCertifcate)){
                 $HasSSLCertificatesPerNode = $false
-                if($i -gt 0){
-                    throw "Please check and try again. All Nodes in the configuration don't have a SSL Certificate!"
-                }
+                $Roles = $ConfigurationParamsHashtable.AllNodes[$i].Role
+                if(-not(($Roles.Count -eq 1 -and -not($Roles -icontains "DataStore" -or $Roles -icontains "FileShare")) -or ($Roles.Count -eq 2 -and -not((Compare-Object $Roles @("FileShare", "DataStore")).count -eq 0)))
+                {
+                    if($i -gt 0){
+                        throw "Please check and try again. All Nodes in the configuration don't have a SSL Certificate!"
+                    }
+                }                
             }
         }
-
+        
         $CommonNodeToAddForPlainText = @{
             NodeName = "*"
             PSDscAllowPlainTextPassword = $true
